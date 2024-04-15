8a23eee8f898cc6bca52082be9da5c94239e41dd
Denis Sukhin <88856699+xFlodz@users.noreply.github.com>
2024-04-15 16:28:25 +0300

    timeline

--- post_sort.py
+++ post_sort.py
@@ -3,11 +3,12 @@ def post_sort(posts, left_date, right_date):
     stack = []
     print(posts)
     for post in posts:
-        print(post.left_date)
-        if post.left_date >= left_date:
-            stack.append(post)
-        else:
-            pass
+        if post != None:
+            print(post.left_date)
+            if post.left_date >= left_date:
+                stack.append(post)
+            else:
+                pass
     for post in stack:
         if post.right_date <= right_date:
             sorted_posts_list.append(post)
