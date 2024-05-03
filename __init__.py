637339fd6270cdbb419771d5f04f378ea084f9df
Denis Sukhin <88856699+xFlodz@users.noreply.github.com>
2024-05-03 17:55:40 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
@@ -12,5 +11,4 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['ROLES'] = ['admin', 'poster']
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 manager = LoginManager(app)
